b14c210c3cad76c5642c4b1f8f8b221a881e5f73
jonah <73760377+jonerrr@users.noreply.github.com>
2022-07-10 09:59:23 -0400

    fix

--- src/discord/checks.ts
+++ src/discord/checks.ts
@@ -10,7 +10,10 @@ export async function check(
       : await users.findOne({ _id: id });
 
   return {
-    sentiment: typeof data.sentiment === "boolean" ? data.sentiment : true,
+    sentiment:
+      data.sentiment && typeof data.sentiment === "boolean"
+        ? data.sentiment
+        : true,
     quiz: typeof data.popQuiz === "boolean" ? data.popQuiz : true,
     cooldown: data.cooldown,
   };
